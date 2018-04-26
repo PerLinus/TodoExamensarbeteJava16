386b3cd7d7c805d8eae312e00a4268bb70a4021a
PerLinus <linus.nilsson83@gmail.com>
2018-04-26 15:51:34 +0200

    Initial commit

---
+++ app/src/main/java/nilsson83/linus/todo/MainActivity.java
@@ -0,0 +1,13 @@
+package nilsson83.linus.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
